d6f91773259a1fefd14bbe05c41fcf9c1aed9966
Stathis Kamperis <ekamperi@gmail.com>
2021-02-09 03:03:08 +0000

    Add nll() and get_loss_and_grads()

--- ml_utils.py
+++ ml_utils.py
@@ -27,3 +27,23 @@ def integrate_pdf(dist, x_range, y_range, n_samples=1000):
     pdf = dist.prob(tXY)
 
     return tf.reduce_sum(pdf) * dA
+
+
+@tf.function
+def nll(dist, x_train):
+    """Calculates the negative log-likelihood for a given distribution
+    and a data set.
+    """
+    return -tf.reduce_mean(dist.log_prob(x_train))
+
+
+@tf.function
+def get_loss_and_grads(dist, x_train):
+    """Returns a tuple of (loss, gradients) for a given distribution
+    and a dat set. The loss is the negative log-likelihood.
+    """
+    with tf.GradientTape() as tape:
+        tape.watch(dist.trainable_variables)
+        loss = nll(dist, x_train)
+        grads = tape.gradient(loss, dist.trainable_variables)
+    return loss, grads
